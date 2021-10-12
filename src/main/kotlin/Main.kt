dd92511ef02874d26a7eafec9f1f47b7cfb941a0
Nora <Norah.Nahedh.ruh.k@tuwaiq.edu.sa>
2021-10-12 14:52:38 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+//const val apple =50
+fun main(args: Array<String>) {
+//    var name : String ="Nora"
+//    var num : Int = 5
+//    var flag : Boolean = true
+//    var bf : String ? = null
+//    println("My name is  ${name}  my fav number is ${num}  Im I developer ! ${flag}")
+//    //if example
+//    if (num == 5 && flag==true){
+//        println("hi baby")
+//    }
+//    else {
+//        println("bye baby")
+//    }
+//    //if example
+//    var num1 :Int = 6
+//    if (num > num1){
+//        println("num greater than num1")
+//    }else{
+//        println("num1 greater than num2")
+//    }
+   //example
+//    var score = 5
+//    score+=3
+//    println(score)
+//  var dozon=  apple/ 12
+//    var remain = apple % 12
+//    println("we have ${dozon} dozon of apple " +"\n"+ "the remain apple is ${remain}")
+
+}
